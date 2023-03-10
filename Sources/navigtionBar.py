993b29c96161a751786310474fb69126e8a5e352
Rafaeyl <123087885+Rafayel1992@users.noreply.github.com>
2023-03-10 15:08:16 +0400

    Update navigtionBar.py

--- Sources/navigtionBar.py
+++ Sources/navigtionBar.py
@@ -1,25 +1,25 @@
 from selenium.webdriver.common.by import By
 from Sources.basePage import  BasePage
 
-class NavigtionBarLocators():
-    serchFieldLocators = (By.ID,"twotabsearchtextbox")
-    clickSerchBottonLocators = (By.ID, "nav-search-submit-button")
+class NavigtionBarLocator():
+    serchFieldLocator = (By.ID,"twotabsearchtextbox")
+    clickSerchBottonLocator = (By.ID, "nav-search-submit-button")
     clickCartBotton = (By.ID,"nav-cart-text-container")
     clickHomePage = (By.ID,"nav-logo-sprites")
 
 
-class NavigtionBar(NavigtionBarLocators,BasePage):
+class NavigtionBar(NavigtionBarLocator,BasePage):
     def __init__(self,driver):
         super().__init__(driver)
         self.driver = driver
 
     def fill_serch_field(self,txet):
-        serchFieldElement =self.find_element(*(self.serchFieldLocators))
+        serchFieldElement =self.find_element(*(self.serchFieldLocator))
         serchFieldElement.clear()
         serchFieldElement.send_keys(txet)
 
     def click_to_serch_botton(self):
-        searchBottomElement = self.find_element(*(self.clickSerchBottonLocators))
+        searchBottomElement = self.find_element(*(self.clickSerchBottonLocator))
         searchBottomElement.click()
 
 
