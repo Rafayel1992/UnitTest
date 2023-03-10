8a71bf78c5ce84bf951f8c1c87eae319caf1c49b
Rafayel1992 <123087885+Rafayel1992@users.noreply.github.com>
2023-03-10 15:28:23 +0400

    unitTest\\ home work
    # Conflicts:
    #	Sources/cartPage.py
    #	Sources/produkt.py
    #	Sources/serchResalt.py
    #	Sources/signInPage.py

--- Sources/signInPage.py
+++ Sources/signInPage.py
@@ -1,38 +1,38 @@
 from Sources.basePage import BasePage
 from selenium.webdriver.common.by import By
 
-class SignInPageLocators():
-    signInFieldLocators = (By.ID,"nav-link-accountList")
-    loginFieldLocators = (By.ID,"ap_email")
-    continueFieldLocators = (By.ID,"continue")
-    passwordFieldLocators = (By.ID,"ap_password")
-    clickPasswordFieldLocators = (By.ID,"signInSubmit")
+class SignInPageLocator():
+    signInFieldLocator = (By.ID,"nav-link-accountList")
+    loginFieldLocator= (By.ID,"ap_email")
+    continueFieldLocator = (By.ID,"continue")
+    passwordFieldLocator = (By.ID,"ap_password")
+    clickPasswordFieldLocator = (By.ID,"signInSubmit")
 
-class SignInPage(SignInPageLocators,BasePage):
+class SignInPage(SignInPageLocator,BasePage):
     def __init__(self,driver):
         super().__init__(driver)
         self.driver = driver
 
     def click_to_signIn_button(self):
-         ButtonElement= self.find_element(*(self.signInFieldLocators))
+         ButtonElement= self.find_element(*(self.signInFieldLocator))
          ButtonElement.click()
 
     def fill_login_field(self,login):
-        loginPageElement = self.find_element(*(self.loginFieldLocators))
+        loginPageElement = self.find_element(*(self.loginFieldLocator))
         loginPageElement.clear()
         loginPageElement.send_keys(login)
 
     def click_to_continue_botton(self):
-        loginBottonElement = self.find_element(*(self.continueFieldLocators))
+        loginBottonElement = self.find_element(*(self.continueFieldLocator))
         loginBottonElement.click()
 
     def fill_password_field(self,password):
-        passwoedPageElement = self.find_element(*(self.passwordFieldLocators))
+        passwoedPageElement = self.find_element(*(self.passwordFieldLocator))
         passwoedPageElement.clear()
         passwoedPageElement.send_keys(password)
 
     def click_continue_botton(self):
-        passwordBottonElement = self.find_element(*(self.clickPasswordFieldLocators))
+        passwordBottonElement = self.find_element(*(self.clickPasswordFieldLocator))
         passwordBottonElement.click()
 
 
