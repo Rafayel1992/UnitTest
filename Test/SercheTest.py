ebd2f1ce469c09a3d8a26d62c163dabf618e41c0
Rafayel1992 <123087885+Rafayel1992@users.noreply.github.com>
2023-03-14 16:48:20 +0400

    unitTest\\ home work
    # Conflicts:
    #	Sources/cartPage.py
    #	Sources/produkt.py
    #	Sources/serchResalt.py
    #	Sources/signInPage.py

--- Test/SercheTest.py
+++ Test/SercheTest.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 import unittest
 from Sources.Nav_Bar.navigtionBar import NavigtionBar
 from Sources.Nav_Bar.serchResalts import ProduktPage
+from Sources.ProduktsPage.signInPage import SignInPage
+import time
 
 class SercheTest (unittest.TestCase):
     def setUp(self) -> None:
@@ -13,9 +15,18 @@ class SercheTest (unittest.TestCase):
         self.driver.get("https://www.amazon.com/")
         self.navigtionBarObj = NavigtionBar(self.driver)
         self.produktObj = ProduktPage(self.driver)
+        self.signInPageObj = SignInPage(self.driver)
 
 
     def test_sercheResalt(self):
+        self.signInPageObj.mouse_move()
+        time.sleep(3)
+        self.signInPageObj.click_to_signIn_button()
+        self.signInPageObj.fill_login_field('rafayel1624@gmail.com')
+        self.signInPageObj.click_to_continue_botton()
+        time.sleep(5)
+        self.signInPageObj.fill_password_field('2594011624')
+        self.signInPageObj.click_signIn_botton()
         self.navigtionBarObj.fill_serch_field("toys")
         self.navigtionBarObj.click_to_serch_botton()
         self.produktObj.click_to_serch_produkt()
